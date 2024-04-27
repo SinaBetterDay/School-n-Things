99bd2c3282e0e89819371d9b482ff413aa8c224f
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2024-04-27 14:22:30 -0700

    Create #9 Hex Instructions

---
+++ ASSEMBLY/#9 Hex Instructions.asm
@@ -0,0 +1,55 @@
+unchecked!!
+
+a)	BX:3000
+SF = 0
+ZF = 0
+
+b)	BX:FB77
+SF = 1
+ZF = 0
+
+c)	BX:CB77
+SF = 1
+ZF = 0
+
+d)	BX:058A
+
+e)	AX:0005
+SF = 0
+ZF = 0
+
+f)	AX:FFFF
+SF = 1
+ZF = 0
+
+g)	AX:058A
+SF = 0
+ZF = 0
+
+i)	CX:7145
+
+j)	CX:EFFA
+SF = 1
+ZF = 0
+
+k)	CX:8CB2
+SF = 1
+ZF = 0
+
+l)	CX:6348
+SF = 0
+ZF = 0
+	
+n)	DX:495C
+
+o)	DX:F6EB
+SF = 1
+ZF = 0
+
+p)	DX:2480
+SF = 0
+ZF = 0		
+
+q)	DX:D26B
+SF = 1
+ZF = 0
